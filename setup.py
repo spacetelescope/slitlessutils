78220ffdcf4356e35f729e5fa503407a33e30db5
Ricky O'Steen <rosteen@stsci.edu>
2023-03-28 17:02:36 -0400

    Working on fixing installation

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
 from glob import glob
 import numpy as np
 import os
-import requests
-from setuptools import setup,Extension
-
+from setuptools import setup, Extension
 
 
 if __name__ == '__main__':
 
-    #filename='latest.tar.gz'
-    #url=''
-    #path=''
-    #r=requsts.get(url,allow_redirects=True)
-    #with open(os.path.join(path,filename),'wb') as fp:
-    #    fp.write(r.content)
-
-    
     tokens=('slitlessutils','core','wfss','config','polyclip')
     ext=Extension('.'.join(tokens)+'.cpolyclip',
                   glob(os.path.join(*tokens,'src','*.c')),
                   include_dirs=[os.path.join(*tokens,'include'),
                                 np.get_include()])
-    
+
     setup(ext_modules=[ext])
