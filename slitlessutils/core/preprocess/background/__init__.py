41b8d4abeeb58e13a18e4ce870bfc996ab12760e
Russell Ryan <rryan@stsci.edu>
2023-04-24 17:25:47 -0400

    updated background module for master-sky, poly1d

--- slitlessutils/core/preprocess/background/__init__.py
+++ slitlessutils/core/preprocess/background/__init__.py
@@ -1,2 +1,5 @@
-from .cartesian import cartesian
-from .mastersky import mastersky
+from .background import Background
+#from .cartesian import cartesian
+#from .mastersky import mastersky
+#from .poly1dsky import poly1dsky
+#from .poly2dsky import poly2dsky
