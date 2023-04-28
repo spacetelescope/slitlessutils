632c900a1c9577dff8736b567df4a10538ae6545
Duy Nguyen <duytnguyendtn.open@gmail.com>
2023-04-28 11:36:02 -0400

    Automatically download the config files on package import

--- slitlessutils/__init__.py
+++ slitlessutils/__init__.py
@@ -6,3 +6,7 @@ from . import examples
 
 from .logger import start_logging, end_logging, initialize_logger
 LOGGER = initialize_logger()
+
+from slitlessutils.config import download_config_files
+
+download_config_files()
