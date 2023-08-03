ae895cba4fe10c0f146a0ee70cecc8f147e229a5
Larry Bradley <larry.bradley@gmail.com>
2023-08-03 09:41:46 -0400

    Require pypolclip as a runtime dependency

--- setup.py
+++ setup.py
@@ -1,15 +1,3 @@
-from glob import glob
-import numpy as np
-import os
-from setuptools import setup, Extension
+from setuptools import setup
 
-
-if __name__ == '__main__':
-
-    tokens=('slitlessutils','core','wfss','config','polyclip')
-    ext=Extension('.'.join(tokens)+'.cpolyclip',
-                  glob(os.path.join(*tokens,'src','*.c')),
-                  include_dirs=[os.path.join(*tokens,'include'),
-                                np.get_include()])
-
-    setup(ext_modules=[ext])
+setup()
