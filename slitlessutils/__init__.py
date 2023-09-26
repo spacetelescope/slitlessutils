eb3efb598e5988ae74fe4a17d273dfe1a815c8a8
Larry Bradley <larry.bradley@gmail.com>
2023-09-26 13:50:53 -0400

    Update __init__

--- slitlessutils/__init__.py
+++ slitlessutils/__init__.py
@@ -1,8 +1,13 @@
 # main imports
-from .info import __version__   # noqa: F401
+from . import examples  # noqa: F401
 from .core import *  # noqa: F401, F403
 from .ds9regions import *  # noqa: F401, F403
-from . import examples  # noqa: F401
 
-from .logger import start_logging, end_logging, initialize_logger  # noqa: F401
+try:
+    from .version import version as __version__
+except ImportError:
+    __version__ = ''
+
+from .logger import end_logging, initialize_logger, start_logging  # noqa: F401
+
 LOGGER = initialize_logger()
