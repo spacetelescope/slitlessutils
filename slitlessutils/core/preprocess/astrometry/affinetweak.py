3900d9583cd9ad2fecc6229d5811a67f030a20ed
Russell Ryan <rryan@stsci.edu>
2023-10-04 09:53:51 -0400

    updating and codifying the astrometry code.  clarifying details in do… (#43)
    * updating and codifying the astrometry code.  clarifying details in docstrings and rst files
    
    * fixed pep8 fails
    
    * Update slitlessutils/core/preprocess/astrometry/affinetweak.py
    
    * implementing rickys suggestions
    
    * fixed pep8 problem
    
    ---------
    
    Co-authored-by: Ricky O'Steen <39831871+rosteen@users.noreply.github.com>

--- slitlessutils/core/preprocess/astrometry/affinetweak.py
+++ slitlessutils/core/preprocess/astrometry/affinetweak.py
@@ -1,28 +1,94 @@
 from astropy.io import fits
 import numpy as np
-import os
+
+from ....logger import LOGGER
+from . import utils
 
 
 class AffineTweak(dict):
+    """
+    Class to contain an affine perturbation to the Astrometry of a fits image
+    between two different WCS solutions present in a file.
+
+    This assumes the "classic" astrometric formulation of a CD (or PC) matrix
+    and a CRVAL vector.  The affine transformation is a simple vector difference
+    of the CRVAL keywords and a matrix product between the CD matrices.  This
+    permits for skew, rotation, and translation between the two WCSs.  Now, the
+    method computes the multiplicative matrix of the CD-matrix and vector
+    difference between the final WCS and the initial WCS:
+
+    .. math::
+       A = CD_{0} CD_{1}^{-1}
+
+    This is then matrix-multiplied into the CD matrix for a different image to
+    update it.  In general, this matrix A is not simply a rotation matrix as
+    the astrometric tweaks may include skew or pixel-size changes.  In a similar
+    fashion, the difference in the CRVALs between the final and initial WCSs:
+
+    .. math::
+      \\Delta CRVAL = CRVAL_{1} - CRVAL_{0}
+
+    In general, these tweaks are typically <~0.1 arcsec.
+
+
+    Parameters
+    ----------
+    reffile : str
+        The file name of the image to use as a reference.
+
+    key0 : str, optional
+        The WCS key for the initial WCS.  This is usually the "older" WCS.
+        Note, this is not case-sensitive (only capitals are used).
+        Default is "A"
+
+    key1 : str, optional
+        The WCS key for the final WCS.  This is usually the "newer" WCS.
+        Note, this is not case-sensitive (only capitals are used).
+        Default is '' (the empty string).
+
+    Notes
+    -----
+    This class inherits from ``dict`` to store individual tweaks for each
+    detector, in the case of instruments that have multiple detectors (e.g.
+    ACS WFC).  The keyword/value pairs for the dict will be the extension
+    name/version and a dict of data for the perturbations.  Users are
+    highly discouraged from manually manipulating those contents.
+
+    Examples
+    --------
+    The usual work flow would be to pass a direct image that has been
+    tweaked to some optimal solution (e.g. referenced to Gaia) to derive
+    the perturbations.  Then apply those perturbations to a grism image
+    that has not been similarly updated.
+
+    >>> tweak = AffineTweak('directimage_flt.fits') # doctest: +SKIP
+    >>> newfile = tweak('grismimage_flt.fits') # doctest: +SKIP
+
+    """
+
     def __init__(self, reffile, key0='A', key1=''):
 
         self.reffile = reffile
-        self.key0 = key0
-        self.key1 = key1
+        self.key0 = key0.upper()
+        self.key1 = key1.upper()
 
         with fits.open(self.reffile, mode='readonly') as hdul:
             for hdu in hdul:
+                # get the extension name
                 extname = hdu.header.get('EXTNAME')
                 extver = hdu.header.get('EXTVER')
+                ext = (extname, extver)
 
                 if extname == 'SCI':
-                    cd0 = self.get_cd(hdu.header, self.key0)
-                    crval0 = self.get_crval(hdu.header, self.key0)
+                    # get the old WCS
+                    cd0 = utils.get_cd(hdu.header, self.key0)
+                    crval0 = utils.get_crval(hdu.header, self.key0)
 
-                    cd1 = self.get_cd(hdu.header, self.key1)
-                    crval1 = self.get_crval(hdu.header, self.key1)
+                    # get the new WCS
+                    cd1 = utils.get_cd(hdu.header, self.key1)
+                    crval1 = utils.get_crval(hdu.header, self.key1)
 
-                    ext = (extname, extver)
+                    # create and fill a dict of the data
                     self[ext] = {}
                     self[ext]['A'] = np.dot(cd1, np.linalg.inv(cd0))
                     self[ext]['d'] = crval1-crval0
@@ -30,56 +96,65 @@ class AffineTweak(dict):
                     self[ext]['wcsname1'] = hdu.header[f'WCSNAME{self.key1}']
 
     def __call__(self, datfile, inplace=False, newfile=None):
+        """
+        Main method to apply the tweaks to a given file.
+
+        Parameters
+        ----------
+        datfile : str
+            The name of the file to be tweaked.  This should have the same
+            number and names of the extensions as the 'reffile' used to
+            instantiate the object.
+
+        inplace : bool, optional
+            A flag that this file should be updated in place.  Note, if
+            the WCS in a file is overwritten (ie. `inplace=True`), then
+            the original WCS is unrecoverable.  Default is False.
+
+        newfile : str or None, optional
+            See the file `utils.py` for more details.  Default is None.
+
+        Returns
+        -------
+        filename : str
+            The name of the output file that was updated.
+
+        """
 
         mode = 'update' if inplace else 'readonly'
         with fits.open(datfile, mode=mode) as hdul:
             for ext, data in self.items():
 
-                cd = self.get_cd(hdul[ext].header, self.key0)
-                crval = self.get_crval(hdul[ext].header, self.key0)
+                wcsname0 = hdul[ext].header[f'WCSNAME{self.key0}']
+                if wcsname0 != data['wcsname0']:
+                    LOGGER.warning('mismatches in the WCSNAMEs')
+
+                cd = utils.get_cd(hdul[ext].header, self.key0)
+                crval = utils.get_crval(hdul[ext].header, self.key0)
 
                 cd = np.dot(data['A'], cd)
                 crval = crval+data['d']
 
-                self.set_cd(hdul[ext].header, cd, self.key1)
-                self.set_crval(hdul[ext].header, crval, self.key1)
+                utils.set_cd(hdul[ext].header, cd, self.key1)
+                utils.set_crval(hdul[ext].header, crval, self.key1)
+
+                # add the tweak to the header
+                hdul[ext].header.set('DCRVAL1', value=data['d'][0])
+                hdul[ext].header.set('DCRVAL2', value=data['d'][1])
+                hdul[ext].header.set('A1_1', value=data['A'][0, 0])
+                hdul[ext].header.set('A1_2', value=data['A'][0, 1])
+                hdul[ext].header.set('A2_1', value=data['A'][1, 0])
+                hdul[ext].header.set('A2_2', value=data['A'][1, 1])
+
+                # add some more to header
+                hdul[ext].header.set('WCSTWEAK', value=True,
+                                     comment='Affine tweaked by slitlessutils')
+                hdul[ext].header.set('WCSTYPE', value='affine upgrade')
 
             hdul[0].header.add_history('Affine tweaked astrometry')
 
-            if inplace:
-                outfile = datfile
-            else:
-                if newfile is None:
-                    base = os.path.splitext(os.path.basename(datfile))[0]
-                    newfile = f'{base}_twcs.fits'
-                hdul.writeto(newfile, overwrite=True)
-                outfile = newfile
-        return outfile
+            outfile = utils.new_filename(datfile, inplace=inplace, suffix='twcs')
+            if not inplace:
+                hdul.writeto(outfile, overwrite=True)
 
-    @staticmethod
-    def get_cd(hdr, key):
-        cd = np.array([[hdr[f'CD1_1{key}'], hdr[f'CD1_2{key}']],
-                       [hdr[f'CD2_1{key}'], hdr[f'CD2_2{key}']]],
-                      dtype=float)
-        return cd
-
-    @staticmethod
-    def set_cd(hdr, cd, key):
-        hdr[f'CD1_1{key}'] = cd[0, 0]
-        hdr[f'CD1_2{key}'] = cd[0, 1]
-        hdr[f'CD2_1{key}'] = cd[1, 0]
-        hdr[f'CD2_2{key}'] = cd[1, 1]
-
-    @staticmethod
-    def get_crval(hdr, key):
-        crval = np.array([hdr[f'CRVAL1{key}'], hdr[f'CRVAL2{key}']],
-                         dtype=float)
-        return crval
-
-    @staticmethod
-    def set_crval(hdr, crval, key):
-        hdr[f'CRVAL1{key}'] = crval[0]
-        hdr[f'CRVAL2{key}'] = crval[1]
-
-    # def __str__(self):
-    #    print(
+            return outfile
--- slitlessutils/core/preprocess/astrometry/downgrade_wcs.py
+++ slitlessutils/core/preprocess/astrometry/downgrade_wcs.py
@@ -1,19 +1,96 @@
-import os
-
 from astropy.io import fits
 
 from ....logger import LOGGER
+from . import utils
 
 
-def downgrade_wcs(imgfile, key='A', newfile=None, inplace=False):
+def _downgrade_wcs(imgfile, key, mode, newfile, inplace):
     """
-    Method to take WCS in a direct image that has been tied to Gaia, and
-    downgrade it to a previous WCS
+    Helper function to actually execute the downgrading
 
     Parameters
     ----------
     imgfile : str
-        the name of the direct image file
+        Path to a file to downgrade
+
+    key : str
+        WCS key to downgrade from.  Will replace the current solutions
+
+    mode : str
+        The file mode see `astropy.io.fits.open()`
+
+    newfile : str
+        The output filename
+
+    inplace : bool
+        Flag that the update should happen in place
+
+    Returns
+    -------
+    outfile : str
+        The name of the file that contains the new WCS
+
+    Notes
+    -----
+    It is not expected for one to directly call this function, though nothing prohibits it.
+
+    """
+
+    if not isinstance(imgfile, str):
+        LOGGER.warning('imgfile must be a string')
+        return None
+
+    LOGGER.info(f'downgrading WCS in {imgfile} to WCSNAME{key}')
+    with fits.open(imgfile, mode=mode) as hdul:
+
+        for ext, hdu in enumerate(hdul):
+
+            wcsname = hdu.header.get('WCSNAME')
+
+            # get the current WCS
+            crval = utils.get_crval(hdu.header, '')
+            cd = utils.get_cd(hdu.header, '')
+
+            # get the old WCS
+            wcsname2 = hdu.header[f'WCSNAME{key}']
+            crval2 = utils.get_crval(hdu.header, key)
+            cd2 = utils.get_cd(hdu.header, key)
+
+            # swap the WCSs
+            hdul[ext].header['WCSNAME'] = wcsname2
+            utils.set_crval(hdul[ext].header, crval2)
+            utils.set_cd(hdul[ext].header, cd2)
+
+            hdul[ext].header['WCSNAME'] = wcsname
+            utils.set_crval(hdul[ext].header, crval)
+            utils.set_cd(hdul[ext].header, cd)
+
+            # update with some comments
+            hdul[ext].header.set('WCSTWEAK', value=True,
+                                 comment='WCS tweaked by slitlessutils')
+            hdul[ext].header.set('WCSTYPE', value='downgrade')
+
+        hdul[0].header.add_history('Downgraded astrometry')
+
+        # get a new file
+        outfile = utils.new_filename(imgfile, newfile=newfile, inplace=inplace,
+                                     suffix=f'WCSNAME{key}')
+        if not inplace:
+            hdul.writeto(outfile, overwrite=True)
+
+    return outfile
+
+
+def downgrade_wcs(inputs, key='A', newfile=None, inplace=False):
+    """
+    Method to take WCS in a direct image that has been tied to Gaia, and
+    downgrade it to a previous WCS.
+
+    Parameters
+    ----------
+    inputs : str, list, or tuple
+        If this is a str, then it is the name of the file to be downgrade.
+        If it is a list or tuple, then it is many such filenames.
 
     key : str, optional
         the WCS key for the old WCS to downgrade to. default is 'A'
@@ -23,11 +100,7 @@ def downgrade_wcs(imgfile, key='A', newfile=None, inplace=False):
         ie. without creating a new file.  Default is False
 
     newfile : str, optional
-        The name of the new file with the tweaked WCS.  If inplace==False,
-        then this is ignored.  If inplace==True, then this name will be used.
-        If newfile == None, then a filename will be generated as:
-
-        f'{}_twcs.fits'
+        See the file `utils.py` for more details.  Default is None
 
     Returns
     -------
@@ -36,65 +109,12 @@ def downgrade_wcs(imgfile, key='A', newfile=None, inplace=False):
 
     """
 
-    LOGGER.info(f'downgrading WCS in {imgfile} to WCSNAME{key}')
-
     mode = 'update' if inplace else 'readonly'
-    with fits.open(imgfile, mode=mode) as hdul:
-        # do some error checking
-        obstype = hdul[0].header['OBSTYPE']
-        if obstype != 'IMAGING':
-            LOGGER.warning(f'direct image obstype ({obstype}) is not imaging.')
-            return
 
-        for ext, hdu in enumerate(hdul):
-
-            wcsname = hdu.header.get('WCSNAME')
-
-            if wcsname and (wcsname.lower().find('gaia') != -1):
-
-                # get the current WCS
-                crvals = [hdu.header['CRVAL1'], hdu.header['CRVAL2']]
-                cd = [[hdu.header['CD1_1'], hdu.header['CD1_2']],
-                      [hdu.header['CD2_1'], hdu.header['CD2_2']]]
-
-                # get the old WCS
-                wcsname2 = hdu.header[f'WCSNAME{key}']
-                crvals2 = [hdu.header[f'CRVAL1{key}'], hdu.header[f'CRVAL2{key}']]
-                cd2 = [[hdu.header[f'CD1_1{key}'], hdu.header[f'CD1_2{key}']],
-                       [hdu.header[f'CD2_1{key}'], hdu.header[f'CD2_2{key}']]]
-
-                # swap the WCSs
-                hdul[ext].header['WCSNAME'] = wcsname2
-                hdul[ext].header['CRVAL1'] = crvals2[0]
-                hdul[ext].header['CRVAL2'] = crvals2[1]
-                hdul[ext].header['CD1_1'] = cd2[0][0]
-                hdul[ext].header['CD1_2'] = cd2[0][1]
-                hdul[ext].header['CD2_1'] = cd2[1][0]
-                hdul[ext].header['CD2_2'] = cd2[1][1]
-
-                hdul[ext].header[f'WCSNAME{key}'] = wcsname
-                hdul[ext].header[f'CRVAL1{key}'] = crvals[0]
-                hdul[ext].header[f'CRVAL2{key}'] = crvals[1]
-                hdul[ext].header[f'CD1_1{key}'] = cd[0][0]
-                hdul[ext].header[f'CD1_2{key}'] = cd[0][1]
-                hdul[ext].header[f'CD2_1{key}'] = cd[1][0]
-                hdul[ext].header[f'CD2_2{key}'] = cd[1][1]
-
-                # update with some comments
-                hdul[ext].header.set('WCSTWEAK', value=True,
-                                     comment='WCS tweaked by slitlessutils')
-                hdul[ext].header.set('WCSTYPE', value='downgrad',
-                                     comment='WCS downgraded to pre-Gaia')
-
-        # figure out how to write the file and get a variable to return
-        if inplace:
-            outfile = imgfile
-
-        else:
-            if newfile is None:
-                # get a new filename
-                base = os.path.splitext(os.path.basename(imgfile))[0]
-                newfile = f'{base}_WCSNAME{key}.fits'
-            hdul.writeto(newfile, overwrite=True)
-            outfile = newfile
-    return outfile
+    if isinstance(inputs, str):
+        return _downgrade_wcs(inputs, key, mode, newfile, inplace)
+    elif isinstance(inputs, (list, tuple)):
+        return [_downgrade_wcs(i, key, mode, newfile, inplace) for i in inputs]
+    else:
+        LOGGER.warning(f"Invalid datatype ({type(inputs)}) for input.")
+        return
