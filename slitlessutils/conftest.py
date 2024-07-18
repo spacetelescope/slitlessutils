660de31015936f43d765cd1208e4c8bd238c474e
Ricky O'Steen <39831871+rosteen@users.noreply.github.com>
2024-07-18 13:24:47 -0400

    Add ACS grism unit test (#73)
    * Working on ACS grism unit test
    
    * Uncomment the astrodrizzle run
    
    * Codestyle
    
    * Max pin photutils for now
    
    * Try adding h5py to build-system requirements
    
    * Put test in jail
    
    * Codestyle
    
    * Temporarily pin some things due to deprecation warnings
    
    * Ignore stsci deprecation warning in pytest.ini
    
    * brew install hdf5 in CI
    
    * Remove h5py from build system
    
    * Try brew install here
    
    * Remove 3.9 test for now
    
    * Add remote data to mac test for now
    
    * Put setuptools pin in correct place, allow 3.9 failure for now
    
    * Missing comma
    
    * Add newline at end of file

--- slitlessutils/conftest.py
+++ slitlessutils/conftest.py
@@ -1,6 +1,20 @@
+import os
+import pytest
 from slitlessutils import config
 
 cfg = config.Config()
 
 # download the latest reference files
 reffile = cfg.retrieve_reffiles(update=True)
+
+
+# Copied over from https://github.com/spacetelescope/ci_watson
+@pytest.fixture(scope='function')
+def _jail(tmp_path):
+    """Perform test in a pristine temporary working directory."""
+    old_dir = os.getcwd()
+    os.chdir(tmp_path)
+    try:
+        yield str(tmp_path)
+    finally:
+        os.chdir(old_dir)
