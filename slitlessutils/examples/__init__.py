9202e49642630f31ca6ae4b0959aa6e1155b9b9d
Russell Ryan <rryan@stsci.edu>
2026-03-01 22:46:38 -0500

    Commit fixes the multi-orient extraction, adds two example files, and fixes minor things

--- slitlessutils/examples/__init__.py
+++ slitlessutils/examples/__init__.py
@@ -1,5 +1,7 @@
 from . import acswfc  # noqa: F401
+from . import extract_multi  # noqa: F401
 from . import ring  # noqa: F401
+from . import simulate  # noqa: F401
 from . import starfield  # noqa: F401
 from . import wfc3ir  # noqa: F401
 from . import wfc3uvis  # noqa: F401
