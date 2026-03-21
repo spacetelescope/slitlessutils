fixed units in output and trimmed zero data

slitlessutils/core/photometry/sed.py
@@ -255,29 +255,6 @@ class SED:
         self._data['npix'][self.count:self.count + nadd] = npix
         self.count += nadd
 
-    def for_output(self):
-        """
-        Method to rename the internal numpy columns for a more descriptive
-        output in the fits tables
-
-        Returns
-        -------
-        dat : `np.ndarray()`
-           This will be a structured numpy array with descriptive column names
-        """
-
-        # get the data locally
-        dat = self._data
-
-        # get the new names by remapping the internal names to better
-        # external names for the ouptut files
-        new_names = tuple(self.EXTKEYS[inkey] for inkey, dtype in self.DTYPE)
-
-        # reset the outputs
-        dat.dtype.names = new_names
-
-        return dat
-
     def __bool__(self):
         """
         Method to enable to boolean testing based on the count
@@ -783,27 +760,39 @@ class SED:
             np.savetxt(filename, self.data, delimiter=delimiter, fmt=self.FORMAT,
                        header=delimiter.join(self._data.dtype.names))
 
-    def as_HDU(self, **kwargs):
+    def as_HDU(self, header=None):
         """
-        Method to package the spectrum in to an `astropy.io.fits.BinTableHDU`
+        Method to rename the internal numpy columns for a more descriptive
+        output in the fits tables
 
-        Parameters
-        ----------
-        kwargs : dict, optional
-            Keyword/value pairs set as header keyword/value pairs
-
+        Returns
+        -------
+        hdu : `fits.astropy.io.BinTableHDU()`
+           This will be a structured numpy array with descriptive column names
         """
 
-        fluxunits = Config().fluxunits
+        # get the data locally and trim the padding
+        data = self.data
 
-        hdu = fits.BinTableHDU(self.data)
+        # get the new names by remapping the internal names to better
+        # external names for the ouptut files
+        new_names = tuple(self.EXTKEYS[inkey] for inkey, dtype in self.DTYPE)
 
-        hdu.header.set('TUNIT1', value='angstrom', after='TFORM1')
-        hdu.header.set('TUNIT2', value=fluxunits, after='TFORM2')
-        hdu.header.set('TUNIT3', value=fluxunits, after='TFORM3')
-        hdu.header.set('TUNIT4', value=fluxunits, after='TFORM4')
-        hdu.header.set('TUNIT5', value='number', after='TFORM5')
+        # reset the outputs
+        data.dtype.names = new_names
 
-        for k, v in kwargs.items():
-            hdu.header[k] = v
+        # put into an HDU
+        hdu = fits.BinTableHDU(data, header=header)
+
+        # get units of the spectrum
+        cfg = Config()
+        fluxunit = f'{cfg.fluxscale} {cfg.fluxunits}'
+
+        # update the output units
+        hdu.data.columns['WAVELENGTH'].unit = 'Angstrom'
+        hdu.data.columns['FLUX'].unit = fluxunit
+        hdu.data.columns['UNCERTAINTY'].unit = fluxunit
+        hdu.data.columns['CONTAMINATION'].unit = fluxunit
+        hdu.data.columns['NMEASUREMENTS'].unit = ''
+
         return hdu

slitlessutils/core/sources/source.py
@@ -10,7 +10,6 @@ from astropy.wcs import WCS
 from astropy.wcs import utils as wcsutils
 from skimage.segmentation import expand_labels
 
-from ...config import Config
 from ...logger import LOGGER
 from ..utilities import headers, indices
 from .dispersedregion import DispersedRegion
@@ -480,81 +479,82 @@ class Source(list):
         If the source is compound, then it will be written with 3-dimensional
             WCS.
         """
-        hdr = fits.Header()
-        hdr['EXTNAME'] = (str(self.segid),)
 
-        hdr['SEGID'] = (self.segid, 'Segmentation ID')
-        hdr['GRPID'] = (kwargs.get('group', self.grpid), 'Group ID')
-        hdr['RA'] = (self.adc[0], 'Right Ascension (deg)')
-        hdr['DEC'] = (self.adc[1], 'Declination (deg)')
-        hdr['X'] = (self.xyc[0], 'X barycenter')
-        hdr['Y'] = (self.xyc[1], 'Y barycenter')
-        hdr['IMGFLUX'] = (self.flux, 'instrumental flux in direct image')
-        hdr['IMGMAG'] = (self.mag, 'AB magnitude in direct image')
-        hdr['IMGFNU'] = (self.fnu, 'flux in erg/s/cm2/Hz in direct image')
-        hdr['NPIXELS'] = (self.npixels, 'total number of extracted pixels')
-        hdr['WHTTYPE'] = (self.whttype, 'Type of source profile weights')
-        hdr['WAVEUNIT'] = ('AA', 'Unit on the wavelength')
-        hdr['FLUXUNIT'] = (f'{Config().fluxunits} erg cm-2 s-1 AA-1', 'Unit on the flamb spectrum')
-        hdr['FLUXTYPE'] = ('flam', 'Is spectrum `flam` or `fnu` units')
+        # make an output header
+        header = fits.Header()
+
+        # make some basic keywords
+        header['EXTNAME'] = (str(self.segid),)
+
+        header['SEGID'] = (self.segid, 'Segmentation ID')
+        header['GRPID'] = (kwargs.get('group', self.grpid), 'Group ID')
+        header['RA'] = (self.adc[0], 'Right Ascension (deg)')
+        header['DEC'] = (self.adc[1], 'Declination (deg)')
+        header['X'] = (self.xyc[0], 'X barycenter')
+        header['Y'] = (self.xyc[1], 'Y barycenter')
+        header['IMGFLUX'] = (self.flux, 'instrumental flux in direct image')
+        header['IMGMAG'] = (self.mag, 'AB magnitude in direct image')
+        header['IMGFNU'] = (self.fnu, 'flux in erg/s/cm2/Hz in direct image')
+        header['NPIXELS'] = (self.npixels, 'total number of extracted pixels')
+        header['WHTTYPE'] = (self.whttype, 'Type of source profile weights')
+        header['FLUXTYPE'] = ('flam', 'Is spectrum `flam` or `fnu` units')
 
         if self.whttype == 'fitprofile':
-            hdr['WHTPROF'] = (self.profile, 'The analytic profile for the weights')
-        hdr['NREGIONS'] = (self.nregions, 'number of spectral regions')
-        hdr['AREA'] = (self.npixels * self.pixelarea, 'source area (arcsec2)')
-        headers.add_stanza(hdr, 'Source Properties', before='SEGID')
+            header['WHTPROF'] = (self.profile, 'The analytic profile for the weights')
+        header['NREGIONS'] = (self.nregions, 'number of spectral regions')
+        header['AREA'] = (self.npixels * self.pixelarea, 'source area (arcsec2)')
+        headers.add_stanza(header, 'Source Properties', before='SEGID')
 
-        hdr['BCKSUB'] = (self.local_back, 'Was local background in direct image subtracted')
-        hdr['BCKSIZE'] = (self.backsize, 'Size of background annulus (in pix)')
-        hdr['BCKVAL'] = (self.background, 'Background level in direct image')
-        hdr['BCKLOSIG'] = (self.nsig[0], 'N sigma for low level')
-        hdr['BCKHISIG'] = (self.nsig[1], 'N sigma for high level')
-        headers.add_stanza(hdr, 'Direct Image Background', before='BCKSUB')
+        header['BCKSUB'] = (self.local_back, 'Was local background in direct image subtracted')
+        header['BCKSIZE'] = (self.backsize, 'Size of background annulus (in pix)')
+        header['BCKVAL'] = (self.background, 'Background level in direct image')
+        header['BCKLOSIG'] = (self.nsig[0], 'N sigma for low level')
+        header['BCKHISIG'] = (self.nsig[1], 'N sigma for high level')
+        headers.add_stanza(header, 'Direct Image Background', before='BCKSUB')
 
         if self.is_compound:
-            hdr['NAXIS'] = (3, 'number of WCS axes')
-            hdr['NAXIS1'] = self.wcs._naxis[0]
-            hdr['NAXIS2'] = self.wcs._naxis[1]
-            # hdr['NAXIS3']=nlam
+            header['NAXIS'] = (3, 'number of WCS axes')
+            header['NAXIS1'] = self.wcs._naxis[0]
+            header['NAXIS2'] = self.wcs._naxis[1]
+            # header['NAXIS3']=nlam
             raise RuntimeError("gotta get nlam")
 
-            hdr['CTYPE1'] = self.wcs.ctype[0]
-            hdr['CTYPE2'] = self.wcs.ctype[1]
-            hdr['CTYPE3'] = 'wavelength (A)'
+            header['CTYPE1'] = self.wcs.ctype[0]
+            header['CTYPE2'] = self.wcs.ctype[1]
+            header['CTYPE3'] = 'wavelength (A)'
 
-            hdr['CRPIX1'] = self.wcs.crpix[0]
-            hdr['CRPIX2'] = self.wcs.crpix[1]
-            hdr['CRPIX3'] = 1
+            header['CRPIX1'] = self.wcs.crpix[0]
+            header['CRPIX2'] = self.wcs.crpix[1]
+            header['CRPIX3'] = 1
 
-            hdr['CRVAL1'] = self.wcs.crval[0]
-            hdr['CRVAL2'] = self.wcs.crval[1]
-            # hdr['CRVAL3']=lam0
+            header['CRVAL1'] = self.wcs.crval[0]
+            header['CRVAL2'] = self.wcs.crval[1]
+            # header['CRVAL3']=lam0
 
-            hdr['LTV1'] = self.ltv[0]
-            hdr['LTV2'] = self.ltv[1]
+            header['LTV1'] = self.ltv[0]
+            header['LTV2'] = self.ltv[1]
 
-            hdr['CD1_1'] = self.wcs.cd[0, 0]
-            hdr['CD1_2'] = self.wcs.cd[0, 1]
-            hdr['CD2_1'] = self.wcs.cd[1, 0]
-            hdr['CD2_2'] = self.wcs.cd[1, 1]
-            # hdr['CD3_3']=dlam
+            header['CD1_1'] = self.wcs.cd[0, 0]
+            header['CD1_2'] = self.wcs.cd[0, 1]
+            header['CD2_1'] = self.wcs.cd[1, 0]
+            header['CD2_2'] = self.wcs.cd[1, 1]
+            # header['CD3_3']=dlam
 
-            hdr['CUNIT1'] = self.wcs.cunit[0]
-            hdr['CUNIT2'] = self.wcs.cunit[1]
-            hdr['CUNIT3'] = 'A'
+            header['CUNIT1'] = self.wcs.cunit[0]
+            header['CUNIT2'] = self.wcs.cunit[1]
+            header['CUNIT3'] = 'A'
 
-            hdr['DISPAXIS'] = (3, 'dispersion axis')
-            headers.add_stanza(hdr, 'WORLD-COORDINATE SYSTEM', before='NAXIS')
+            header['DISPAXIS'] = (3, 'dispersion axis')
+            headers.add_stanza(header, 'WORLD-COORDINATE SYSTEM', before='NAXIS')
 
             # set the data
-            dat = np.zeros((hdr['NAXIS3'], hdr['NAXIS2'], hdr['NAXIS1']),
-                           dtype=float)
+            dim = (header['NAXIS3'], header['NAXIS2'], header['NAXIS1'])
+            dat = np.zeros(dim, dtype=float)
             raise NotImplementedError("three-dimensional extraction is not yet supported")
-            hdu = fits.ImageHDU(dat, hdr)
+            hdu = fits.ImageHDU(dat, header)
         else:
-            # set the data as a single spectrum
-            dat = self[0].sed.for_output()
-            hdu = fits.BinTableHDU(dat, hdr)
+            hdu = self[0].sed.as_HDU(header=header)
+
         return hdu
 
     def image_coordinates(self, x, y, dtype=None):